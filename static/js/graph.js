19c6c0cba37d52be59b950a93ecb5a1c1d8fab65
Melanie Milsted <melanie.milsted@gmail.com>
2018-09-15 17:51:07 +0000

    addition of nav bar and css styling

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,18 +1,17 @@
 queue()
    .defer(d3.csv, "data/wise.csv")
-   .await(makeGraphs);
-   
-function makeGraphs(error, wiseData) {
+  .await(makeGraphs);
+  
+  function makeGraphs(error, wiseData) {
     var ndx = crossfilter(wiseData);
-       
+      
     show_discoveryYear(ndx);
     show_PHA_data(ndx);
        
-    show_hMag_to_period_correlation(ndx);
-    
     show_class(ndx);
-       
+    
     dc.renderAll();
+    
 }
    
 function show_discoveryYear(ndx) {
@@ -29,8 +28,8 @@ function show_PHA_data(ndx) {
     var group = dim.group();
        
     dc.barChart("#PHA-data")
-      .width(400)
-      .height(300)
+      .width(500)
+      .height(400)
       .margins({top: 10, right: 50, bottom: 30, left: 50})
       .dimension(dim)
       .group(group)
@@ -41,54 +40,17 @@ function show_PHA_data(ndx) {
       .yAxis().ticks(20);
 }
    
-function show_hMag_to_period_correlation(ndx) {
-    
-     var PHAColors = d3.scale.ordinal()
-        .domain(["Y", "N", "n/a"])
-        .range(["red", "green", "yellow"]);
-    var periodDim = ndx.dimension(dc.pluck('period'));
-     var period_dim = ndx.dimension(function(d){
-        return [d.hMag, d.period, d.PHA];
-    });
-    
-   var periodGroup = periodDim.group();
-    var min_period = periodDim.bottom(1)[0].period;
-    var max_period = periodDim.top(1)[0].period;
-    
-     /**var hMagDim = ndx.dimension(dc.pluck('hMag'));
-     var hMag_dim = ndx.dimension(function(d){
-        return [d.hMag, d.period, d.PHA];
-    });
-     var hMagGroup = hMagDim.group();
-    var min_hMag = hMagDim.bottom(1)[0].hMag;
-    var max_hMag = hMagDim.top(1)[0].hMag;**/
 
-    dc.scatterPlot("#orbit-correlation")
-        .width(768)
-        .height(480)
-        .x(d3.time.scale().domain([min_period,max_period]))
-        /**.y(d3.scale.linear().domain([min_hMag,max_hMag]))**/
-        .brushOn(false)
-        .symbolSize(8)
-        .clipPadding(10)
-        .yAxisLabel("H Magnitude")
-        .xAxisLabel("Orbital Period")
-        .dimension(period_dim)
-        .group(group)
-}
 
-function show_class(ndx){
+function show_class(ndx) {
 
 var class_dim = ndx.dimension(dc.pluck('class'));
-var total_asterids_per_class = class_dim.group();
+var total_asteroids_per_class = class_dim.group();
 dc.pieChart('#orbit-class-chart')
-            .height(330)
-            .radius(90)
+            .height(530)
+            .radius(290)
             .transitionDuration(1500)
             .dimension(class_dim)
-            .group(total_asterids_per_class);
+            .group(total_asteroids_per_class);
 
 }
-
-
-
